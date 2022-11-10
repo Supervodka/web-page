d538d62126bfe8cf65cd4c1160115dad55ed61f7
Кулик Роман Викторович <Kulik_R@ipps.by>
2022-11-10 16:51:41 +0300

    first step

---
+++ web-page/src/App.js
@@ -0,0 +1,78 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='left-bar'>
+    <div className="instagram-logo">
+      instagram
+    </div>
+
+
+    <div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/house1.png"/>
+            <div className='names-of-razdel'>
+  Главная
+  </div>
+  </div>
+
+
+  <div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/lupa.png"/>
+            <div className='names-of-razdel'>
+  Поисковый запрос
+  </div>
+</div>
+
+<div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/compass.png"/>
+            <div className='names-of-razdel'>
+              Интересное
+            </div>
+
+
+</div>
+
+<div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/direct.png"/>
+            <div className='names-of-razdel'>
+                Сообщения
+            </div>
+
+</div>
+
+<div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/like.png"/>
+            <div className='names-of-razdel'>
+                Уведомления
+            </div>
+
+
+</div>
+
+<div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/create.png"/>
+            <div className='names-of-razdel'>
+              Создать
+            </div>
+
+  
+</div>
+
+<div className= 'left-side-bar'>
+            <img alt="Важная картинка " src="../images/house1.png"/>
+            <div className='names-of-razdel'>
+               Профиль
+            </div>
+
+           
+</div>
+
+
+</div>
+    </>
+
+  );
+}
+
+export default App;
